da77f2d68702e1546c57b7c4df6db755f0199758
Jasminephannd <Jasminephannd@gmail.com>
2024-02-01 22:55:54 +1100

    Make changes to the logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,7 @@ export const NavBar = () => {
   return (
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand as="a" href="/" onClick={() => window.location.reload()}>
           <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
